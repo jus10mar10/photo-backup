d8386a8aefb84540f658282739d36123cabbdd07
Justin Martin <justinmartin50@gmail.com>
2024-08-04 17:11:24 -0400

    update to copy over video files too

--- scrub.py
+++ scrub.py
@@ -12,13 +12,13 @@ def generate_unique_name(file_path):
     unique_name = hash_md5.hexdigest() + '_' + str(uuid.uuid4())
     return unique_name
 
-def scrub_and_upload_images(src_dir, dest_dir, image_extensions=['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']):
+def scrub_and_upload_files(src_dir, dest_dir, file_extensions):
     """
-    Scrub images from the source directory and upload them to the destination directory.
+    Scrub files from the source directory and upload them to the destination directory.
 
     :param src_dir: Source directory containing backup disc CDs
-    :param dest_dir: Destination directory to upload image files
-    :param image_extensions: List of image file extensions to consider
+    :param dest_dir: Destination directory to upload files
+    :param file_extensions: List of file extensions to consider
     """
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
@@ -26,7 +26,7 @@ def scrub_and_upload_images(src_dir, dest_dir, image_extensions=['.jpg', '.jpeg'
 
     for root, _, files in os.walk(src_dir):
         for file in files:
-            if any(file.lower().endswith(ext) for ext in image_extensions):
+            if any(file.lower().endswith(ext) for ext in file_extensions):
                 src_file_path = os.path.join(root, file)
                 unique_name = generate_unique_name(src_file_path)
                 ext = os.path.splitext(file)[1]
@@ -38,10 +38,12 @@ def scrub_and_upload_images(src_dir, dest_dir, image_extensions=['.jpg', '.jpeg'
                 else:
                     print(f"File {dest_file_path} already exists. Skipping.")
 
-    print("Image scrubbing and uploading completed.")
+    print("File scrubbing and uploading completed.")
 
 # Example usage
 source_directory = 'D:/'
 destination_directory = 'G:/My Drive/Photo Backup'
+file_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', 
+                   '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm']
 
-scrub_and_upload_images(source_directory, destination_directory)
+scrub_and_upload_files(source_directory, destination_directory, file_extensions)
